101b9476fec4a287bb873d313c3a82f7f57e42c1
Gregory Hawman <g.hawman@gmail.com>
2020-04-14 16:24:02 -0700

    finished mvp, set up my migration up and down, set up the databasse at data/car-dealer.db3, added .get and .post to create and read in the cars api, Postman works at getting the api and post to the api.

--- api/server.js
+++ api/server.js
@@ -6,5 +6,42 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/cars', (req, res) => {
+    db.select('*')
+    .from('cars')
+    .then((cars) => {
+        res.json(cars);
+    })
+    .catch((error) => {
+        res.status(500).json({ message: 'Server Error, cannot retrieve cars', error });
+      });
+})
+
+server.get('/cars/:id', (req, res) => {
+    db('cars')
+      .where({ id: req.params.id })
+      .first()
+      .then((car) => {
+        if (car) {
+          res.status(200).json({ data: car });
+        } else {
+          res.status(500).json({ message: 'Server Error, car not found' });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: 'Server Error', error });
+      });
+  });
+
+  server.post('/cars', (req, res) => {
+      db('cars')
+        .insert(req.body)
+        .then((car) => {
+            res.status(201).json(car)
+        })
+        .catch((error) => {
+            res.status(500).json({ message: 'Server Error, failed to add car', error })
+        });
+  });
 
 module.exports = server;
--- data/migrations/20200414152523_create-cars-table.js
+++ data/migrations/20200414152523_create-cars-table.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.integer('VIN')
+          .unique()
+          .notNullable();
+        tbl.string('make')
+          .notNullable();
+        tbl.string('model')
+          .notNullable();
+        tbl.integer('milage')
+          .notNullable();
+        tbl.string('transmission type')
+          .nullable();
+        tbl.string('status of title')
+          .nullable();
+      });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('cars');
 };
